a7f7f67a34fff6748e0736629f60ed96f4e3e5e4
Charles Rea <charles.rea@ghyston.com>
2019-12-02 10:43:44 +0000

    Problem 1

---
+++ 1.fsx
@@ -0,0 +1,13 @@
+﻿open System.IO
+
+let rec getFuel mass =
+    let fuel = max (mass / 3 - 2) 0
+    if fuel = 0 then 0 else fuel + getFuel fuel
+
+let getTotalFuel =
+    File.ReadLines("./1_input.txt")
+        |> Seq.map int
+        |> Seq.map getFuel
+        |> Seq.sum
+
+printf "%d" getTotalFuel
